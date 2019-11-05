e2fd0a45ee3fd8b072476134917b8cb44410024f
disizali <a.hassssani@gmail.com>
2019-11-05 16:26:48 +0330

    add index route

--- frontend/components/Splash.js
+++ frontend/components/Splash.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import { Row, Col, Button, Container } from "reactstrap";
-import Icon from "./Icons";
+import Icon from "./Icons/Index";
 export class Splash extends Component {
   constructor(props) {
     super(props);
